68914814ddc4932b6953873f70e3a447273cc027
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 00:05:44 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
+require "pry"
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood #creates getter and setter methods 
+  attr_reader :name
+  
+  @@all = []
+    
+   def initialize(name, owner)   #Dog and a Cat are initialized with a name and an Owner object.
+   #(cat.owner.name) seems that cat is the instance of the cat class. owner is a getter method. It looks like association already took place. 
+    @@all << self 
+   
+    @name = name 
+    @owner = owner # sets the cat object setter method(owner) to equal the new owner that was just created 
+    @mood = "nervous"
+    
+   end 
+  
+   def self.all
+    @@all 
+   end 
+   
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species #can have a name, cannot change owner's name. Same case for species
+  
+  @@all = []
+  
+  def initialize(name)
+    @@all << self
+    @name = name 
+    @species = "human"
+    
+  end 
+  
+  def say_species 
+    "I am a #{self.species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count
+    self.all.length
+  end 
+  
+  def self.reset_all
+    self.all.clear
+  end 
+  
 end
