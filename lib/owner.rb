7fb4490883083344f9695d09243a0dd3b4cfd7d4
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-08 01:02:20 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,29 +1,73 @@
+require "pry"
 class Owner
   attr_reader :name, :species #can have a name, cannot change owner's name. Same case for species
-  
+
   @@all = []
-  
+
   def initialize(name)
-    @@all << self
-    @name = name 
+
+    @name = name
     @species = "human"
-    
-  end 
-  
-  def say_species 
+    @@all << self
+
+  end
+
+  def say_species
     "I am a #{self.species}."
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
+  end
+
+  def self.all
+    @@all
+  end
+
   def self.count
     self.all.length
-  end 
-  
+  end
+
   def self.reset_all
     self.all.clear
-  end 
-  
+  end
+
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+    #binding.pry
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+    #binding.pry
+  end
+
+  def buy_cat(cat_name)
+    # this will create a new cat object and set it's name to the provided argument
+
+    new_cats = Cat.new(cat_name, self)
+  end
+
+  def buy_dog(dog_name)
+    # this will create a new cat object and set it's name to the provided argument
+
+    new_dog = Dog.new(dog_name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each{|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each{|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    #this includes cats and dogs
+    self.dogs.each{|dog| dog.mood = "nervous"}
+    self.dogs.each{|dog| dog.owner = nil}
+    self.cats.each{|cat| cat.mood = "nervous"}
+    self.cats.each{|cat| cat.owner = nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
