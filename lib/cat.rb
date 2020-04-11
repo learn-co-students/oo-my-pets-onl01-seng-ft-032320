666652c8fea0b3a6fc7c5377872e797b110e0b1e
edmundo papi <edmundopierre@yahoo.com>
2020-04-11 18:29:29 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,22 +3,14 @@ class Cat
   attr_accessor :mood, :owner
   attr_reader :name
   @@all = []
-  def initialize(cat, owner)
-    @cat = cat
+  def initialize(name, owner)
+    @name = name
     @owner = owner
     @mood = "nervous"
     @@all << self
   end
-   def self.all
+  def self.all
      @@all
-   end
-
-      end
-
-
-    end
+     #binding.pry
   end
-
-
-    end
 end
