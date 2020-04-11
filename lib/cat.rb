4db5fb27bc4c244c3e1a82a3413ce3112d9f9054
edmundo papi <edmundopierre@yahoo.com>
2020-04-11 17:04:39 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
 class Cat
   # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  @@all = []
+  def initialize(cat, owner)
+    @cat = cat
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+   def self.all
+     @@all
+   end
+
+      end
+
+
+    end
+  end
+
+
+    end
 end
