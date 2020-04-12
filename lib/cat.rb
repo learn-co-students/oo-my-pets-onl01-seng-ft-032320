8656981860facc7a7f78a597a58f384f604013b7
Jorge Ramirez <joramirez96@gmail.com>
2020-04-12 01:22:20 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,29 @@
 class Cat
   # code goes here
+  attr_accessor  :owner, :mood
+
+
+  @@cats = []
+
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    owner.cats << self
+    @mood = mood
+    @@cats << self
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@cats
+  end
+
+
+
+
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood
+
+
+  @@dogs = []
+
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+    owner.dogs << self
+    @@dogs << self
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@dogs
+  end
 end
